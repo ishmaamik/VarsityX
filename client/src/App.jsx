61f0229e1e1948efc9435a8ecd9f5a76d4876120
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-05-25 02:20:03 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,11 +65,11 @@ function AppContent() {
         <Route
           path="/home"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <Home />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
@@ -77,66 +77,65 @@ function AppContent() {
         <Route
           path="/marketplace"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <MarketplaceHome />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/marketplace/buy"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <BuyPage />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/marketplace/sell"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <SellPage />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/marketplace/listing/:id"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <ListingDetail />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/marketplace/cart"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <Cart />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/marketplace/category/:category"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <BuyPage />
               </Layout>
-            </ProtectedRoute>
           }
         />
 
@@ -155,99 +154,99 @@ function AppContent() {
         <Route
           path="/my-listings"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <MarketplaceHome />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/favorites"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <MarketplaceHome />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/messages"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <Messages />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/notifications"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <div>Notifications Component</div>
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/profile"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <div>Profile Component</div>
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/scam"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <ImageUploader />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/settings"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <div>Settings Component</div>
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/safe-meetups"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <SafeMeetups />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
         <Route
           path="/price-advisor"
           element={
-            <ProtectedRoute>
+            
               <Layout>
                 <PriceAdvisorPage />
               </Layout>
-            </ProtectedRoute>
+            
           }
         />
 
